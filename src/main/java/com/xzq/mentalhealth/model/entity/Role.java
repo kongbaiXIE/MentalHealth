acbe231259f96f3be571c76ddb1291ffc152057c
kongbaiXIE <2498529257@qq.com>
2023-01-17 12:31:12 +0800

    [A] 权限模块基本完成

---
+++ src/main/java/com/xzq/mentalhealth/model/entity/Role.java
@@ -0,0 +1,62 @@
+package com.xzq.mentalhealth.model.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 角色表
+ * @TableName role
+ */
+@TableName(value ="role")
+@Data
+public class Role implements Serializable {
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 名称
+     */
+    @TableField(value = "name")
+    private String name;
+
+    /**
+     * 备注
+     */
+    @TableField(value = "comment")
+    private String comment;
+
+    /**
+     * 角色唯一标识
+     */
+    @TableField(value = "flag")
+    private String flag;
+
+    /**
+     * 创建时间
+     */
+    @TableField(value = "createTime")
+    private Date createTime;
+
+    /**
+     * 
+     */
+    @TableField(value = "updateTime")
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableField(value = "isDelete")
+    private Integer isDelete;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/xzq/mentalhealth/service/RoleMenuService.java
@@ -0,0 +1,13 @@
+package com.xzq.mentalhealth.service;
+
+import com.xzq.mentalhealth.model.entity.RoleMenu;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 谢志强
+* @description 针对表【role_menu(角色菜单关系)】的数据库操作Service
+* @createDate 2023-01-16 10:30:06
+*/
+public interface RoleMenuService extends IService<RoleMenu> {
+
+}
